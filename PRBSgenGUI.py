29c4de57b0c35154406ccbbed3914f8c70642966
alfordsibanda <40764101+alfordsibanda@users.noreply.github.com>
2018-11-21 14:43:26 +0200

    Update PRBSgenGUI.py

--- PRBSgenGUI.py
+++ PRBSgenGUI.py
@@ -24,15 +24,13 @@ def CurrentFrequency(frequency):
 
 
 def CurrentPRBS(prbs):
-    prbsLookup = {'4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, '11': 11,
-    		  '12': 12, '13': 13, '14': 14}
+    prbsLookup = {'4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, '11': 11, '12': 12, '13': 13, '14': 14}
     prbs = prbsLookup[prbs]
     return str(prbs)
 
 
 def CurrentPeriod(period):
-    periodLookup = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, 
-    		    '9': 9, '10': 10}
+    periodLookup = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10}
     period = periodLookup[period] + periodOffset
     return str(period)
 
